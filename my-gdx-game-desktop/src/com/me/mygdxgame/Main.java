c291266bb921345804d224baf04c023c92072668
sgpoza <sgpoza@gmail.com>
2014-02-25 23:52:30 +0100

    my-gdx-game modificado

--- my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -2,16 +2,31 @@ package com.me.mygdxgame;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 
 public class Main 
 {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
 	public static void main(String[] args) 
 	{
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.debug = drawDebugOutline;
+			TexturePacker2.process(settings, "assets-raw/images",
+			"../CanyonBunny-android/assets/images",
+			"canyonbunny.pack");
+		}
+		
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "My Game";
+		cfg.title = "Space Runners";
 		cfg.useGL20 = false;
 		cfg.width = 800;
-		cfg.height = 600;
+		cfg.height = 480;
 		
 		new LwjglApplication(new MyGdxGame(), cfg);
 	}
--- my-gdx-game/src/com/me/mygdxgame/Constants.java
+++ my-gdx-game/src/com/me/mygdxgame/Constants.java
@@ -4,9 +4,8 @@ public class Constants
 {
 	// Visible game world is 5 meters wide
 	public static final float VIEWPORT_WIDTH = 5.0f;
-	
 	// Visible game world is 5 meters tall
 	public static final float VIEWPORT_HEIGHT = 5.0f;
-	
-	// Modificado
+	// Location of description file for texture atlas
+	public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
 }
