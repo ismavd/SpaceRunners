968f7865131c017da13ca76a6940aeed816c1f70
Isma <ismavd@gmail.com>
2014-03-04 17:35:56 +0100

    Subo level

--- my-gdx-game/src/com/packtpub/libgdx/canyonbunny/gamee/Level.java
+++ my-gdx-game/src/com/packtpub/libgdx/canyonbunny/gamee/Level.java
@@ -1,5 +1,7 @@
 package com.packtpub.libgdx.canyonbunny.gamee;
 
+//Version 1
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
