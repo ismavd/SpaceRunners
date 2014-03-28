b397e7ab46ac17f05729aab7c1bb9e865745bddd
sgpoza <sgpoza@gmail.com>
2014-03-28 13:30:35 +0100

    Falta implementar el funcionamiento de los botones (28/03/2014)

--- my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -5,33 +5,35 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 
-public class Main 
-{
+public class Main {
 	private static boolean rebuildAtlas = true;
 	private static boolean drawDebugOutline = false;
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		if (rebuildAtlas) {
 			Settings settings = new Settings();
 			settings.maxWidth = 1024;
 			settings.maxHeight = 1024;
 			settings.debug = drawDebugOutline;
+
 			TexturePacker2.process(settings, "assets-raw/images",
-			"../my-gdx-game-android/assets/images",
-			"canyonbunny.pack");
+					"../my-gdx-game-android/assets/images", "canyonbunny.pack");
 			TexturePacker2.process(settings, "assets-raw/images-ui",
 					"../my-gdx-game-android/assets/images",
 					"canyonbunny-ui.pack");
+			/*TexturePacker2.process(settings, "assets-raw/images",
+					"bin/images", "canyonbunny.pack");
+			TexturePacker2.process(settings, "assets-raw/images-ui",
+					"bin/images",
+					"canyonbunny-ui.pack");*/
 		}
-		
-		
+
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Space Runners";
 		cfg.useGL20 = false;
 		cfg.width = 800;
 		cfg.height = 480;
-		
+
 		new LwjglApplication(new MyGdxGame(), cfg);
 	}
 }
