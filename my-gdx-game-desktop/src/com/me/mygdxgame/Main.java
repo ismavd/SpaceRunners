a2a4468c210dd8736f40213b4c65a70801867970
sgpoza <sgpoza@gmail.com>
2014-05-22 01:27:43 +0200

    Transición, monedas aleatorias, algunos nombres modificados, etc. (22/05/2014)

--- my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -6,17 +6,18 @@ import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 
 public class Main {
-	private static boolean rebuildAtlas = true;
+	private static boolean rebuildAtlas = false;
 	private static boolean drawDebugOutline = false;
 
 	public static void main(String[] args) {
 		if (rebuildAtlas) {
 			Settings settings = new Settings();
+		
 			settings.maxWidth = 1024;
 			settings.maxHeight = 1024;
 			settings.debug = drawDebugOutline;
 
-			/*TexturePacker2.process(settings, "assets-raw/images",
+			TexturePacker2.process(settings, "assets-raw/images",
 					"../my-gdx-game-android/assets/images", "spacerunners.pack");
 			TexturePacker2.process(settings, "assets-raw/images-ui",
 					"../my-gdx-game-android/assets/images",
@@ -29,13 +30,13 @@ public class Main {
 					"bin/images",
 					"spacerunners-ui.pack");
 			TexturePacker2.process(settings, "assets-raw/images-pause",
-					"bin/images", "spacerunners-pause.pack");*/
+					"bin/images", "spacerunners-pause.pack");
 			
 		}
 
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Space Runners";
-		cfg.useGL20 = false;
+		cfg.useGL20 = true;
 		cfg.width = 800;
 		cfg.height = 480;
 
